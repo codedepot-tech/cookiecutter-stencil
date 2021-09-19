2838ed4d84fa364d765e47678d8b879e1f4bad14
Peter Alexander <makeroftools@creativedevelopment.systems>
2021-09-18 21:35:22 -0400

    using npmlock2nix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,67 +1,13 @@
 import os, shutil, sys
 from subprocess import call, run, Popen
 
-SANDBOX_DIR = os.path.realpath(os.path.curdir)
-PROJECT_DIR = os.path.join(SANDBOX_DIR, '{{cookiecutter.project_name}}')
-FRONTEND_DIR = os.path.join(PROJECT_DIR, 'src')
-BACKEND_DIR = os.path.join(PROJECT_DIR, 'backend')
-
-project_js_deps = "{{cookiecutter.project_js_deps}}"
-project_js_dev_deps = "{{cookiecutter.project_dev_deps}}"
-default_js_deps = "(space seperated) BUILTINS ARE.. graphql apollo-client"
-default_js_dev_deps = "(space seperated) BUILTINS ARE.. nodejs npm yarn stencil"
 
 
-if project_js_deps == default_js_deps:
-  project_js_deps = default_js_deps.split(' ')[4:]
-else:
-  project_js_deps = project_js_deps.split(' ')
-  project_js_deps += default_js_deps[4:]
 
-if project_js_dev_deps == default_js_dev_deps:
-  project_js_dev_deps = default_js_dev_deps.split(' ')[4:]
-else:
-  project_js_dev_deps = project_js_dev_deps.split(' ')
-  project_js_dev_deps += default_js_dev_deps.split(' ')[4:]
-
-# print(f"{default_js_deps}")
-# print(f"{project_js_deps}")
-# print("")
-# print(f"{default_js_dev_deps}")
-# print(f"{project_js_dev_deps}")
-
-# sys.exit(0)
-git_commands = [
-  'git init'.split(' '),
-  'git add .'.split(' '),
-  # 'git commit -m \"init\" '.split(' ')
-  'git commit -m "init"'.split(' ')
-]
 
 
 
 if __name__ == '__main__':
 
-  call('npm init stencil'.split(' '))
-
-  for x in ['flake.nix', '.envrc', '.gitignore', 'README.md', 'LICENSE']:
-    shutil.move(f'{x}', f'./{{cookiecutter.project_name}}/{x}')
-
-  os.chdir(PROJECT_DIR)
-  os.mkdir(".direnv")
-
-  for dep in project_js_deps:
-    call(f'npm install {dep}'.split(' '))
-
-  for ddep in project_js_dev_deps:
-    call(f'npm install --save-dev {ddep}'.split(' '))
-
-  call('npm install'.split(' '))
-
-  call('npm audit fix'.split(' '))
-
-  # git commands
-  for cmd in git_commands:
-    call(cmd)
-
-  # call('nix develop'.split(' '))
+  call(f'npm init'.split(' '))
+  call('npm install --save-exact stencil'.split(' '))
--- {{cookiecutter.project_name}}/flake.nix
+++ {{cookiecutter.project_name}}/flake.nix
@@ -6,29 +6,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nnbp.url = "github:serokell/nix-npm-buildpackage";
+    nl2n-src.url = "github:nix-community/npmlock2nix";
+    nl2n-src.flake = false;
   };
 
-  outputs = {self, nixpkgs, nnbp}:
+  outputs = {self, nixpkgs, nl2n-src}:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; overlays = [ nnbp.overlay ]; };
-    in rec
+      pkgs = import nixpkgs { inherit system; };
+      nl2n = import nl2n-src { inherit pkgs; };
+      {{cookiecutter.project_name}} = nl2n.build {
+        src = ./{{cookiecutter.project_name}};
+        installPhase = "cp -r dist $out";
+      };
+    in
       {
-        packages.${system}.{{cookiecutter.project_name}} = pkgs.buildNpmPackage {
-          src = ./.;
-          npmBuild = "npm run build";
-          extraEnvVars = {
-            "PUPPETEER_SKIP_DOWNLOAD=1";
-          };
-        };
+        packages.${system}.{{cookiecutter.project_name}} = {{cookiecutter.project_name}};
 
         defaultPackage.${system} = self.packages.${system}.{{cookiecutter.project_name}};
 
-        devShell.${system} = pkgs.mkShell {
-          buildInputs = [
-            packages.${system}.{{cookiecutter.project_name}}
-          ];
+        devShell.${system} = nl2n.shell {
+          src = ./.;
         };
       };
 }
